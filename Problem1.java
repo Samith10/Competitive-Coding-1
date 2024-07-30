9fe7b0557d6bd9c70f9d253f50e49f6ef73c43f6
Samith10 <samithp98@gmail.com>
2024-07-30 12:42:59 -0400

    Completed CC1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,29 @@
 
+// Time Complexity : O(log n)
+// Space Complexity : 1
+// Did this code successfully run on Leetcode : Yes
+
+// Your code here along with comments explaining your approach in three sentences only
+// Calculate difference of element and the index at low,high, mid position
+// If difference is not equal then element is missing at that side, ignore other side
+
+class MissingNumber {
+    public static int findMissingNumber(int arr[], int size)
+    {
+        int low = 0;
+        int high = size - 1;
+        int mid = 0;
+        while(high-low >= 2){
+            mid = low + (high - low)/2;
+            int midDiff = arr[mid] - mid;
+            int lowDiff = arr[low] - low;
+            if (lowDiff != midDiff){
+                high = mid;
+            }
+            else{
+                low = mid;
+            }
+        }
+        return (arr[low] + arr[high])/2;
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,92 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : N/A
 
+// Your code here along with comments explaining your approach in three sentences only
+// Calculate difference of element and the index at low,high, mid position
+// If difference is not equal then element is missing at that side, ignore other side
+class GFG {
+
+    private int[] Heap;
+    private int size;
+    private int maxsize;
+
+    private static final int FRONT = 1;
+
+    public GFG(int maxsize) {
+        this.maxsize = maxsize;
+        this.size = 0;
+        Heap = new int[this.maxsize + 1];
+        Heap[0] = Integer.MIN_VALUE;
+    }
+
+    private int parent(int pos) {
+        return pos / 2;
+    }
+
+    private int leftChild(int pos) {
+        return (2 * pos);
+    }
+
+    private int rightChild(int pos) {
+        return (2 * pos) + 1;
+    }
+
+    private boolean isLeaf(int pos) {
+        return pos > (size / 2);
+    }
+
+    private void swap(int fpos, int spos) {
+        int tmp = Heap[fpos];
+        Heap[fpos] = Heap[spos];
+        Heap[spos] = tmp;
+    }
+
+    private void minHeapify(int pos) {
+        if (!isLeaf(pos)) {
+            int swapPos = pos;
+            if (rightChild(pos) <= size) {
+                swapPos = Heap[leftChild(pos)] < Heap[rightChild(pos)] ? leftChild(pos) : rightChild(pos);
+            } else {
+                swapPos = leftChild(pos);
+            }
+
+            if (Heap[pos] > Heap[leftChild(pos)] || Heap[pos] > Heap[rightChild(pos)]) {
+                swap(pos, swapPos);
+                minHeapify(swapPos);
+            }
+        }
+    }
+
+    public void insert(int element) {
+        if (size >= maxsize) {
+            return;
+        }
+
+        Heap[++size] = element;
+        int current = size;
+
+        while (Heap[current] < Heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    public void print() {
+        for (int i = 1; i <= size / 2; i++) {
+            System.out.print(
+                    " PARENT : " + Heap[i]
+                    + " LEFT CHILD : " + Heap[2 * i]
+                    + " RIGHT CHILD :" + Heap[2 * i + 1]);
+            System.out.println();
+        }
+    }
+
+    public int remove() {
+        int popped = Heap[FRONT];
+        Heap[FRONT] = Heap[size--];
+        minHeapify(FRONT);
+        return popped;
+    }
+
+}
